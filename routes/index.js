cf0249826f13c0f8c4eb01d45078bd0d7247080a
Tessa Kelly <tessakkelly@gmail.com>
2015-04-10 11:39:23 -0700

    add/update routes to use queries

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../utils/queries.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -13,13 +14,20 @@ router.post('/', function(req, res, next) {
   });
   req.on("end", function() {
     var newRoute = "" + Math.floor(Math.random() * 100) + Date.now();
-    res.status(200).send({id: newRoute});
+    res.status(200).send({url: newRoute});
   });
 });
 
-router.get('/:id', function(req, res, next) {
-  res.send(req.params.id);
+router.get('/forum/:url', function(req, res, next) {
+  db.findOneForum(req, res);
 });
 
+router.post('/forum', function(req, res, next) {
+  res.send("This page is still being built");
+});
+
+router.get('/:url', function(req, res, next) {
+  db.getQuestionsByForum(req, res);
+});
 
 module.exports = router;
